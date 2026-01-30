7a41773f633809ed11fb3adb156b2ed9c2c123d6
Sjini <sjini@gmail.com>
2026-01-30 11:44:22 +0530

    DEMO

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Main } from './shared/main/main';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Main,RouterLink,Home,About,Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
